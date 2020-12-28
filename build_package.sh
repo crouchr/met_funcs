4a3a63d6f55eb9bdbb0b9d6b24a3bf067219b0f3
crouchr <richard.crouch@vonage.com>
2020-12-28 16:04:02 +0000

    First version

--- build_package.sh
+++ build_package.sh
@@ -1,2 +1,6 @@
+# When performing a release:
+# - increment version number in setup.py
+# - run this script
+# - reboot the web-server
 pipenv run python setup.py bdist_wheel
 cp dist/MetFuncs-*.whl /home/crouchr/PycharmProjects/learnage/environments/production/web-server/apache/python-packages/
